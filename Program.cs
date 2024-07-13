d570c553d6d4774442504849444f9c6e044112d1
Marwen <Marwen@DESKTOP-30SGQKT>
2024-07-13 13:28:19 +0200

    Corriger la ConnectionString.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using MetreOr.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add the DefaultConnection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
