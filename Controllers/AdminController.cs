1ea4a775a6bccabf4b9d4e6549c4dcd3a6793f51
Marwen <Marwen@DESKTOP-30SGQKT>
2024-07-16 21:12:47 +0200

    Preparation du Dashboard admin pour verifier les nouveaux utilisateurs.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using MetreOr.Data;
+using MetreOr.ViewModels.AdminVM;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetreOr.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            UserToVerifyViewModel verifyUserList = new UserToVerifyViewModel
+            {
+                UserToVerify = _context.AppUsers.Where(x => x.IsVerified.Equals(false)).ToList()
+            };
+
+            return View(verifyUserList);
+        }
+    }
+}
---
+++ ViewModels/AdminVM/UserToVerifyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MetreOr.Models;
+
+namespace MetreOr.ViewModels.AdminVM
+{
+    public class UserToVerifyViewModel
+    {
+        public IEnumerable<AppUser> UserToVerify { get; set; } = new List<AppUser>();
+    }
+}
